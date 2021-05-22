6a7717b6dc14c24194b91026078f96776c6643f2
ElenaVillano <villaele14@gmail.com>
2021-05-22 17:35:41 -0500

    adelantos en escrito y gráficos

--- r_code/primeros_graficos.R
+++ r_code/primeros_graficos.R
@@ -1,11 +1,13 @@
 library(tidyverse)
+library(patchwork)
+library(ggplot2)
+library(GGally)
 corazones <- read.csv('/Volumes/MemoriaEle/HeavyData/heart_failure_clinical_records_dataset.csv')
 head(corazones)
 summary(corazones)
 dim(corazones)
 names(corazones)
 
-layout(matrix(c()))
 
 
 # Conjunto de datos de registros clínicos de fallas del corazón
@@ -73,3 +75,32 @@ hist(corazones$serum_sodium,breaks = 30,
 
 hist(corazones$creatinine_phosphokinase,breaks = 30, 
      col='blue', border= 'blue4', main='Nivel de la encima CPK en la sangre',ylab=' ')
+
+theme_set(theme_minimal(base_size = 14))
+
+uno <- ggplot(corazones, aes(serum_sodium, serum_creatinine))+
+  geom_point()
+
+dos <- ggplot(corazones, aes(platelets, ejection_fraction))+
+  geom_point()
+
+tres <- ggplot(corazones, aes(platelets, log(creatinine_phosphokinase)))+
+  geom_point()
+uno+dos/tres
+corazones$creatinine_phosphokinase <- log(corazones$creatinine_phosphokinase)
+
+ggpairs(corazones, columns=c(3,5,7,8,9),
+        aes(color=as.factor(diabetes),alpha=0.5))
+#my_cols <- c("#00AFBB", "#E7B800", "#FC4E07")  
+
+
+
+corazones$platelets <- log(corazones$platelets)
+
+
+
+
+
+
+
+
